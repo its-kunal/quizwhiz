d41d355248e099ccc63131ef814cdf5688931a32
Kunal Agrawal <kunal.py3@gmail.com>
2025-04-08 13:13:49 +0530

    feat: add reusable Button component with customizable label and click handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import BgSvg from "@/assets/bg.svg";
+import Button from "@/components/Button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <div className="flex items-center gap-y-4 flex-col">
+        <h1 className="text-3xl">Welcome to QuizWhiz</h1>
+        <p className="text-gray-400">
+          Test your knowledge and learn something new with engaging quizzes on
+          various topics.
+        </p>
+        <Image src={BgSvg} alt="" className="h-72 object-contain" />
+
+        <Link href="/quiz">
+          <Button label="Start Quiz" />
+        </Link>
+      </div>
+    </div>
+  );
 }
